466e9e0f9b216aeb6e48d55b8754a943a14c08b6
carlosgc <tu_email@ejemplo.comgit config --list>
2024-12-15 12:39:19 +0100

    He puesto un par de párrafos con enlace a ellos

---
+++ ProyectoDePrueba/prueba.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <p>Hola me llamo carlos y si clickas en este texto te saldrá otro texto</p>
+    <script>
+        const p = document.querySelector('p');
+        p.addEventListener('click', () => {
+            alert('Has hecho click en el párrafo');
+        });
+    </script>
+
+    <p>Esto es otro párrafo, y si clickas aquí te saldrá otro mensaje</p>
+    <script>
+        const p2 = document.querySelector('p:nth-child(2)');
+        p2.addEventListener('click', () => {
+            alert('Has hecho click en el segundo párrafo');
+        });
+    </script>
+
+</body>
+</html>
